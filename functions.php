60aec8a763a0d9a0545e6bdfc3b9256e5b403349
CraigChilds94 <childscraig17@gmail.com>
2014-09-18 22:25:53 +0100

    Add: Abstract image inclusion

--- functions.php
+++ functions.php
@@ -7,5 +7,19 @@ include_once 'lib/anchor.php';
 set_theme_options(array(
     'header_logo'      => false,
     'logo_src'         => theme_url('public/img/search.png'),
-    'show_admin_links' => false
+    'show_admin_links' => false,
+    'services' => array(
+        (object)array(
+            'icon' => 'car',
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ),
+        (object)array(
+            'icon' => 'car',
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ),
+        (object)array(
+            'icon' => 'car',
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        )
+    )
 ));
--- page-home.php
+++ page-home.php
@@ -11,12 +11,18 @@
         <?php echo page_content(); ?>
     </div>
 </section>
+<?php $services = theme_option('services', false); ?>
+<?php if($services) : ?>
 <section class="content__three--secondary">
     <div class="content__wrap">
-        <?php echo page_content(); ?>
-        <?php echo page_content(); ?>
-        <?php echo page_content(); ?>
+        <?php foreach(range(0, 2) as $i) : ?>
+            <?php $service = $services[$i]; ?>
+            <p>
+                <i class="fa fa-<?php echo $service->icon; ?>"></i>
+                <?php echo $service->text; ?>
+            </p>
+        <?php endforeach; ?>
     </div>
 </section>
-
+<?php endif; ?>
 <?php theme_include('footer'); ?>
